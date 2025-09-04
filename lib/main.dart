3db5bcfd160768fb2adb13df64dd356e7e054e9a
Eakrin <66011212070@msu.ac.th>
2025-09-05 01:47:26 +0700

    leo

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mobile_lotto/page/login_page.dart';
+import 'package:mobile_lotto/page/menu_page.dart';
 import 'package:mobile_lotto/page/register_page.dart';
 
 void main() {
@@ -19,7 +20,7 @@ class MyApp extends StatelessWidget {
           seedColor: const Color.fromARGB(255, 77, 1, 255),
         ),
       ),
-      home: Register_Page(),
+      home: Menu_page(),
     );
   }
 }
